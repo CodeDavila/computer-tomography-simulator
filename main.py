bb7c7255c91718bc6bfa5c52cd30311723d3f094
David Davila <david@Davids-MacBook-Air.local>
2024-05-15 13:17:43 -0600

    Crop animation

--- main.py
+++ main.py
@@ -148,6 +148,8 @@ class MachineMotor:
         self.projections_list = []
         self.reconstruction = np.array((0,0))
         self.reconstruction_tk = None
+        self.cut = None
+        self.cut_tk = None
         self.sync = tk.BooleanVar(value=False)
 
     def transition_to_camera_image(self, step = 0):        
@@ -183,7 +185,7 @@ class MachineMotor:
     def reconstruction_of_the_image(self):
         self.projections = np.array(self.projections_list)
         self.n = self.projections.shape[1]
-        self.th = np.pi - np.deg2rad(np.linspace(1, 180, self.projections.shape[0]))
+        self.th = np.pi/2 - np.deg2rad(np.linspace(1, 180, self.projections.shape[0]))
         self.center = (self.n-1)/2
         x = np.arange(0, self.n)
         y = np.arange(0, self.n)
@@ -212,24 +214,20 @@ class MachineMotor:
         else:
             self.sync.set(not self.sync)
 
-    def crop_reconstruction(self):
-        
-        self.reconstruction = rotate( self.reconstruction,90, reshape=False)
-        cut = np.zeros((self.master.dummy.dummy_height, self.master.dummy.dummy_width))
-        cut = self.reconstruction[
-                int(np.ceil(self.center - self.master.dummy.dummy_height/2)) : (int(np.ceil(self.center + self.master.dummy.dummy_height/2))),
-                int(np.ceil(self.center - self.master.dummy.dummy_width/2)) : (int(np.ceil(self.center + self.master.dummy.dummy_width/2))),
-        ]
-        self.reconstruction = cut
-        cx, cy = self.master.canvas_center()
-        self.reconstruction_tk, x, y = ImageProcessor.process_image_tk(self.reconstruction, cx, cy)
-        self.master.master.after(3000)
-        self.master.update_canvas(self.reconstruction_tk, x, y, self.reconstruction_pass, steps = 0)
-        
-
-    @staticmethod
-    def reconstruction_pass(step = 0):
-        step = step
+    def crop_reconstruction(self, step = 100):
+        if step >= 0:
+            pH = self.master.dummy.camera_height * step // 100
+            pW = self.master.dummy.camera_width * step // 100
+            new_height = self.master.dummy.dummy_height + pH
+            new_width = self.master.dummy.dummy_width + pW
+            self.cut = np.zeros((new_height, new_width))
+            self.cut = self.reconstruction[
+                int(np.ceil(self.center - new_height/2)) : (int(np.ceil(self.center + new_height/2))),
+                int(np.ceil(self.center - new_width/2)) : (int(np.ceil(self.center + new_width/2))),
+            ]
+            cx, cy = self.master.canvas_center()
+            self.cut_tk, x, y = ImageProcessor.process_image_tk(self.cut, cx, cy)
+            self.master.update_canvas(self.cut_tk, x, y, self.crop_reconstruction, steps = step-2)
 
 def main():
     root = tk.Tk()
