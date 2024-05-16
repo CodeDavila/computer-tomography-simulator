ac29466848b695854a1a780eadf890a97b3ea51c
David Alejandro Cerda Dávila <159582092+CodeDavila@users.noreply.github.com>
2024-05-16 13:03:39 -0600

    Update comments constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-YELLOW = "#EE9626"
+# Constants for window dimensions
+WINDOW_WIDTH = 800  # Width of the application window
+WINDOW_HEIGHT = 600  # Height of the application window
 
-
+# Constant for yellow color
+YELLOW = "#EE9626"  # Hexadecimal color code for yellow
