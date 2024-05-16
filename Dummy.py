1e4ae950bd589a8608c68996bc050ee56101b504
David Alejandro Cerda Dávila <159582092+CodeDavila@users.noreply.github.com>
2024-05-16 12:58:35 -0600

    Update comments Dummy.py

--- Dummy.py
+++ Dummy.py
@@ -4,12 +4,17 @@ from skimage import data
 
 class Dummy():
     def __init__(self) -> None:
+        # Initialize the Dummy class
+        # Generate a Shepp-Logan phantom as the dummy image
         self.dummy = data.shepp_logan_phantom()
+        # Define dimensions for resizing
         self.dummy_width = 350
         self.dummy_height = 350
-        self.dummy = resize(self.dummy,(self.dummy_width, self.dummy_height), anti_aliasing=True)
+        # Resize the dummy image
+        self.dummy = resize(self.dummy, (self.dummy_width, self.dummy_height), anti_aliasing=True)
+        # Calculate diagonal length
         diagonal = np.sqrt(self.dummy_height ** 2 + self.dummy_width ** 2)
+        # Calculate camera dimensions based on dummy image dimensions and diagonal length
         self.camera_height = int(np.ceil(diagonal - self.dummy_height) + 2)
         self.camera_width = int(np.ceil(diagonal - self.dummy_width) + 2)
-
-
+        
