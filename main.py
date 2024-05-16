91d675799682b62ec3f2f1e444a9f6dd361f0279
David Alejandro Cerda Dávila <159582092+CodeDavila@users.noreply.github.com>
2024-05-16 13:05:31 -0600

    Update comments main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import tkinter as tk
 from CTMachineApp import CTMachineApp
 
 def main():
+    # Create a Tkinter root window
     root = tk.Tk()
+    # Create an instance of the CTMachineApp class, passing the root window as the master
     app = CTMachineApp(root)
+    # Run the application
     app.run_app()
 
 if __name__ == "__main__":
+    # Call the main function when the script is executed
     main()
-
